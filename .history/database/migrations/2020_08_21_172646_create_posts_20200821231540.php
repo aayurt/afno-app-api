2fe6f9fa2aa4af51ea2982dad49c71cf852386d4
aayurt <ayurtshrestha@gmail.com>
2020-08-21 23:22:46 +0545

    migrations added

---
+++ .history/database/migrations/2020_08_21_172646_create_posts_20200821231540.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('body')->nullable();
+            $table->date('published_at')->nullable();
+            $table->boolean('enabled')->default(false);
+            $table->jsonb('lang')->nullable();
+            $table->integer('popularity')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
