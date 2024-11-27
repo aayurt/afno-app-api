e40e570b598e7fd0cda4e9d10bbf10951feb6532
Aayurt <aayurtshrestha@gmail.com>
2024-11-27 15:16:19 +0000

    fixes

--- resources/views/admin/restaurant/components/form-elements.blade.php
+++ resources/views/admin/restaurant/components/form-elements.blade.php
@@ -29,7 +29,7 @@
     </div>
 </div>
 
-<!-- <div class="form-group row align-items-center" :class="{'has-danger': errors.has('description'), 'has-success': fields.description && fields.description.valid }">
+<div class="form-group row align-items-center" :class="{'has-danger': errors.has('description'), 'has-success': fields.description && fields.description.valid }">
     <label for="description" class="col-form-label text-md-right" :class="isFormLocalized ? 'col-md-4' : 'col-md-2'">{{ trans('admin.restaurant.columns.description') }}</label>
         <div :class="isFormLocalized ? 'col-md-4' : 'col-md-9 col-xl-8'">
         <div>
@@ -37,7 +37,7 @@
         </div>
         <div v-if="errors.has('description')" class="form-control-feedback form-text" v-cloak>@{{ errors.first('description') }}</div>
     </div>
-</div> -->
+</div>
 
 
 <div class="form-group row align-items-center" :class="{'has-danger': errors.has('phone_number'), 'has-success': fields.phone_number && fields.phone_number.valid }">
